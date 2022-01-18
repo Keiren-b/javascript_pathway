2172c693546e909851ed12f23d7b554365bf9d68
Keiren-b <keiren_b@hotmail.com>
2022-01-18 18:30:25 +0100

    update

--- java.js
+++ java.js
@@ -8,32 +8,32 @@ function Book(title, author, pages, read){
     this.read = read
 }
 
-// book_1 = new Book('1', 'J.R.R Tolkien', "295", 'Not Read')
-// book_2 = new Book('2', 'J.K Rowling', '600', 'Read')
-// book_3 = new Book('3', 'C. S. Lewis', '135', 'Read')
-// book_4 = new Book('4', '531', 'Not Read')
-// book_5 = new Book('5', 'J. D. Salinger', '130', 'Not Read')
-// book_6 = new Book('6', 'Harper Lee', '1358', 'Read')
+book_1 = new Book('1', 'J.R.R Tolkien', "295", 'Not Read')
+book_2 = new Book('2', 'J.K Rowling', '600', 'Read')
+book_3 = new Book('3', 'C. S. Lewis', '135', 'Read')
+book_4 = new Book('4',  'James Joyce', '531', 'Not Read')
+book_5 = new Book('5', 'J. D. Salinger', '130', 'Not Read')
+book_6 = new Book('6', 'Harper Lee', '1358', 'Read')
 
 
-// myLibrary.push(book_1)
-// createDivs()
-// myLibrary.push(book_2)
-// createDivs()
-// myLibrary.push(book_3)
-// createDivs()
-// myLibrary.push(book_4)
-// createDivs()
-// myLibrary.push(book_5)
-// createDivs()
-// myLibrary.push(book_6)
-// createDivs()
+myLibrary.push(book_1)
+createDivs()
+myLibrary.push(book_2)
+createDivs()
+myLibrary.push(book_3)
+createDivs()
+myLibrary.push(book_4)
+createDivs()
+myLibrary.push(book_5)
+createDivs()
+myLibrary.push(book_6)
+createDivs()
 
-for (let i=1; i<20; i++){
-    x = new Book (i,i,i,'read')
-    myLibrary.push(x)
-    createDivs()
-}
+// for (let i=1; i<20; i++){
+//     x = new Book (i,i,i,'read')
+//     myLibrary.push(x)
+//     createDivs()
+// }
 
 document.addEventListener('DOMContentLoaded', ()=> {
     document.getElementById('btn').addEventListener('click', addBook)
@@ -70,9 +70,10 @@ function createDivs() {
     pagesCard.textContent='Number of Pages: ' + myLibrary[myLibrary.length-1].pages
     info.appendChild(pagesCard)
 
-    const readCard = document.createElement('h2')
+    const readCard = document.createElement('button')
     readCard.textContent = myLibrary[myLibrary.length-1].read
     info.appendChild(readCard)
+    readCard.addEventListener('click', readToggle)
 
     const rmv = document.createElement('btn')
        rmv.classList.add('rmv')
@@ -106,6 +107,29 @@ function createDivs() {
     }
     
        
+    // let x = document.createElement('button')
+    // x.textContent = 'texttexttext'
+    // container.appendChild(x)
+    // x.addEventListener('click', ()=> {
+    //     let info = document.querySelectorAll('.info')[3]
+    //     info.childNodes[3].textContent = 'HELLO HELLO'
+        
+    // })
+
+
+// myLibrary[x].read = read or not read
+// let readToggle = element.target.parentElement
+// click readToggle button
+//readToggle[4] = read or not read
+
+function readToggle(event){
+
+    if (event.target.textContent === 'Read'){
+        event.target.textContent = 'Not Read';
+    } else {
+        event.target.textContent = 'Read'
+    }
+}
 
 
 
